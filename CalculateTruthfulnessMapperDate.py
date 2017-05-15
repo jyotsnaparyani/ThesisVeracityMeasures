e9db0b63d1fcf929a8ad17ff19a8063b918e563c
jyotsnaparyani <jyotsna.paryani@okstate.edu>
2017-05-15 17:38:32 -0500

    Add files via upload

---
+++ CalculateTruthfulnessMapperDate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+
+
+from textblob import TextBlob
+
+import sys
+
+def readFileandReturnAnArray(fileName, readMode, isLower):
+    myArray=[]
+    with open(fileName, readMode) as readHandle:
+        for line in readHandle.readlines():
+            lineRead = line
+            if isLower:
+                lineRead = lineRead.lower()
+            myArray.append(lineRead.strip().lstrip())
+    readHandle.close()
+    return myArray
+
+
+
+for line in sys.stdin:
+    line = line.strip()
+    if line!="" and line is not None:
+        date = line[-12:]
+        line = line[:-12]
+        tweets_text=line
+        topicfiles = ["foodtopic1", "foodtopic2", "foodtopic3"]
+        for i in topicfiles:
+                alphabet = readFileandReturnAnArray(i, "r", True)
+                topicId = alphabet.pop(0)
+                for symbol in alphabet:
+                        if symbol in tweets_text:
+                                objectivityTruthful = TextBlob(tweets_text)
+                                print '%s\t%s\t%s' % (topicId, objectivityTruthful.polarity,date)
